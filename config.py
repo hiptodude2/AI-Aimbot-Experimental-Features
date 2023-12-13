06f74e9e7dc546bf5740154d329fd7bdd2de4362
Snomn <57030940+SnowHubv2@users.noreply.github.com>
2023-12-13 15:43:55 +0100

    pretty config.py :) and maskSide option
    pretty config.py :) and maskSide option

--- config.py
+++ config.py
@@ -1,44 +1,77 @@
-# Portion of screen to be captured (This forms a square/rectangle around the center of screen)
-screenShotHeight = 320
-screenShotWidth = 320
+# Screen
 
-# For use in games that are 3rd person and character model interferes with the autoaim
-useMask = False
-maskWidth = 80
-maskHeight = 200
+
+# Portion of screen to be captured (This forms a square/rectangle around the center of screen)
+screenShotHeight = 640
+screenShotWidth = 640
+
+
+# Use left or right depending on where the interfering object is, useful for 3rd player models or large guns - JinxTheCatto
+useMask = True
+maskWidth = 180
+maskHeight = 450
+
+# JinxTheCatto
+maskSide = "Left"
+
+
+# What key to press to quit and shutdown the autoaim
+aaQuitKey = "P"
+
+
+# Set to True if you want to get the visuals
+visuals = True
+
+
+
+# AI
+
+
+# Person Class Confidence
+confidence = 0.6
+
 
 # Autoaim mouse movement amplifier
 aaMovementAmp = .4
 
-# Person Class Confidence
-confidence = 0.4
-
-# Determine if the target is within set number of pixels from the center
-# Fortnite = 50 Aimlabs = 12
-aaTriggerBotWidth = 50
-aaTriggerBotHeight = 50
-
-# Automatic Game Detection
-autoGameDetection = False
-gameName = ""
-
-# What key to press to quit and shutdown the autoaim
-aaQuitKey = "Q"
-
-# If you want to main slightly upwards towards the head
-headshot_mode = False
 
 # Displays the Corrections per second in the terminal
 cpsDisplay = True
 
-# Set to True if you want to get the visuals
-visuals = False
+
+
+# Game
+
+
+# Automatic Game Detection
+autoGameDetection = False
+gameName = "Fortnite"
+
+
+
+# Aimbot
+
 
 # Smarter selection of people
 centerOfScreen = True
+
+
+# If you want to main slightly upwards towards the head
+headshot_mode = False
+
+
+# Determine if the target is within set number of pixels from the center
+# Fortnite = 50 Aimlabs = 12
+aaTriggerBotHeight = 45
+aaTriggerBotWidth = 45
+
+
+
+# main_onnx.py only!
+
 
 # ONNX ONLY - Choose 1 of the 3 below
 # 1 - CPU
 # 2 - AMD
 # 3 - NVIDIA
-onnxChoice = 2
+onnxChoice = 3
