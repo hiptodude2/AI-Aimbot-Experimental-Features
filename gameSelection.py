0a8d87fba91085b41a60451a0912c5165de8d274
Hypnoseon <150485506+Hypnoseon@users.noreply.github.com>
2023-12-13 21:07:14 -0500

    Update gameSelection.py

--- gameSelection.py
+++ gameSelection.py
@@ -1,21 +1,12 @@
 import pygetwindow
 import time
 import bettercam
-
-# Could be do with
-# from config import *
-# But we are writing it out for clarity for new devs
 from config import screenShotHeight, screenShotWidth, autoGameDetection, gameName
 
 def gameSelection() -> (bettercam.BetterCam, int, int | None):
     # Selecting the correct game window
     try:
         videoGameWindows = pygetwindow.getAllWindows()
-        print("=== All Windows ===")
-        for index, window in enumerate(videoGameWindows):
-            # only output the window if it has a meaningful title
-            if window.title != "":
-                print("[{}]: {}".format(index, window.title))
 
         if autoGameDetection:
             # Filter windows by the specified game name
@@ -25,82 +16,63 @@ def gameSelection() -> (bettercam.BetterCam, int, int | None):
                 print(f"No windows found for the game '{gameName}'. Exiting.")
                 return None
 
-            if len(filteredWindows) == 1:
-                videoGameWindow = filteredWindows[0]
-            else:
-                # If multiple windows match, let the user choose
-                print("=== Matching Game Windows ===")
-                for index, window in enumerate(filteredWindows):
-                    print(f"[{index}]: {window.title}")
-                try:
-                    userInput = int(input("Please enter the number corresponding to the window you'd like to select: "))
-                    videoGameWindow = filteredWindows[userInput]
-                except ValueError:
-                    print("You didn't enter a valid number. Please try again.")
-                    return None
-                except IndexError:
-                    print("Invalid selection. Please try again.")
-                    return None
+            videoGameWindow = filteredWindows[0] if len(filteredWindows) == 1 else None
+            if videoGameWindow is None:
+                print("Multiple or no matching game windows found. Exiting.")
+                return None
         else:
-            # Manual selection process
+            # Manual selection process with output to the user
+            print("=== All Windows ===")
+            for index, window in enumerate(videoGameWindows):
+                if window.title != "":
+                    print(f"[{index}]: {window.title}")
+
             try:
                 userInput = int(input("Please enter the number corresponding to the window you'd like to select: "))
+                videoGameWindow = videoGameWindows[userInput]
             except ValueError:
                 print("You didn't enter a valid number. Please try again.")
-                return
-            # "save" that window as the chosen window for the rest of the script
-            videoGameWindow = videoGameWindows[userInput]
+                return None
+            except IndexError:
+                print("Invalid selection. Please try again.")
+                return None
 
     except Exception as e:
-        print("Failed to select game window: {}".format(e))
+        print(f"Failed to select game window: {e}")
         return None
 
-    # Activate that Window
+    # Activate the selected Window
     activationRetries = 30
-    activationSuccess = False
     while activationRetries > 0:
         try:
             videoGameWindow.activate()
-            activationSuccess = True
             break
-        except pygetwindow.PyGetWindowException as we:
-            print("Failed to activate game window: {}".format(str(we)))
-            print("Trying again... (you should switch to the game now)")
+        except pygetwindow.PyGetWindowException:
+            activationRetries -= 1
+            time.sleep(1.0)
+            continue
         except Exception as e:
-            print("Failed to activate game window: {}".format(str(e)))
-            print("Read the relevant restrictions here: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setforegroundwindow")
-            activationSuccess = False
-            activationRetries = 0
-            break
-        # wait a little bit before the next try
-        time.sleep(3.0)
-        activationRetries = activationRetries - 1
+            print(f"Failed to activate game window: {e}")
+            return None
 
-    # if we failed to activate the window then we'll be unable to send input to it
-    # so just exit the script now
-    if not activationSuccess:
+    if activationRetries == 0:
+        print("Failed to activate the game window after multiple attempts. Exiting.")
         return None
-
-    print("Successfully activated the game window...")
 
     # Starting screenshoting engine
     left = ((videoGameWindow.left + videoGameWindow.right) // 2) - (screenShotWidth // 2)
-    top = videoGameWindow.top + \
-        (videoGameWindow.height - screenShotHeight) // 2
+    top = videoGameWindow.top + (videoGameWindow.height - screenShotHeight) // 2
     right, bottom = left + screenShotWidth, top + screenShotHeight
-
     region: tuple = (left, top, right, bottom)
 
     # Calculating the center Autoaim box
     cWidth: int = screenShotWidth // 2
     cHeight: int = screenShotHeight // 2
 
-    print(region)
-
     camera = bettercam.create(region=region, output_color="BGRA", max_buffer_len=512)
     if camera is None:
-        print("Your Camera Failed! Ask @Wonder for help in our Discord in the #ai-aimbot channel ONLY: https://discord.gg/rootkitorg")
-        return
-    camera.start(target_fps=120, video_mode=True)
+        print("Your Camera Failed! Ask for help in the support channel.")
+        return None
+    camera.start(target_fps=135, video_mode=True)
 
     return camera, cWidth, cHeight
