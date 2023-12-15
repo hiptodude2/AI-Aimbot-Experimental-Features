e1d2685712d18f9091a1e47d79a7a93bb6edfd45
hiptodude2 <94145364+hiptodude2@users.noreply.github.com>
2023-12-16 00:08:54 +0200

    finally done you can now try it
    fixed and made better 👍

--- main_onnx.py
+++ main_onnx.py
@@ -8,34 +8,19 @@ import win32con
 import pandas as pd
 from utils.general import (cv2, non_max_suppression, xyxy2xywh)
 import torch
-import pygetwindow as gw
-import keyboard  # Added import for capturing keyboard events
+from models.common import DetectMultiBackend  # Add this import for YOLO model
 
 # Add these imports for YOLO model
 from config import aaMovementAmp, aaTriggerBotHeight, aaTriggerBotWidth, fovCircle, fovCircleSize, useMask, maskHeight, maskWidth, aaQuitKey, confidence, headshot_mode, cpsDisplay, visuals, onnxChoice, centerOfScreen
 import gameSelection
-
+import pygetwindow as gw  # Import pygetwindow
+import ctypes
 
 def set_window_on_top(window):
-    window.set_topmost()
-    window.activate()
-
-
-def on_mouse(event, x, y, flags, param):
-    # Handle mouse events here
-    # Example: You can simulate a mouse click at the given (x, y) coordinates
-    if event == cv2.EVENT_LBUTTONDOWN:
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    elif event == cv2.EVENT_LBUTTONUP:
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
-    elif event == cv2.EVENT_RBUTTONUP:
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
-    elif event == cv2.EVENT_MOUSEMOVE:
-        # Simulate mouse movement
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(x * aaMovementAmp), int(y * aaMovementAmp), 0, 0)
-
+    hwnd = ctypes.windll.user32.FindWindowW(None, window.title)
+    ctypes.windll.user32.SetWindowPos(hwnd, -1, 0, 0, 0, 0, 3)  # HWND_TOPMOST = -1
+    ctypes.windll.user32.ShowWindow(hwnd, 1)  # SW_SHOWNOACTIVATE = 1
+    ctypes.windll.user32.SetForegroundWindow(hwnd)
 
 def main():
     # External Function for running the game selection menu (gameSelection.py)
@@ -57,20 +42,17 @@ def main():
 
     so = ort.SessionOptions()
     so.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
-    ort_sess = ort.InferenceSession('yolov5s320Half.onnx', sess_options=so, providers=[onnxProvider])
+    ort_sess = ort.InferenceSession('yolov5s320Half.onnx', sess_options=so, providers=[
+                                    onnxProvider])
 
     # Used for colors drawn on bounding boxes
     COLORS = np.random.uniform(0, 255, size=(1500, 3))
-
-    # Set up the mouse callback for the 'Live Feed' window
-    cv2.namedWindow('Live Feed', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Live Feed', cWidth, cHeight)
-    cv2.setMouseCallback('Live Feed', on_mouse)
 
     # Main loop Quit if Q is pressed
     last_mid_coord = None
     with torch.no_grad():
         while win32api.GetAsyncKeyState(ord(aaQuitKey)) == 0:
+
             # Getting Frame
             npImg = np.array(camera.get_latest_frame())
 
@@ -83,7 +65,7 @@ def main():
                 im = torch.from_numpy(npImg).to('cuda')
                 if im.shape[2] == 4:
                     # If the image has an alpha channel, remove it
-                    im = im[:, :, :3, ]
+                    im = im[:, :, :3,]
 
                 im = torch.movedim(im, 2, 0)
                 im = im.half()
@@ -110,7 +92,8 @@ def main():
 
             im = torch.from_numpy(outputs[0]).to('cpu')
 
-            pred = non_max_suppression(im, confidence, confidence, 0, False, max_det=10)
+            pred = non_max_suppression(
+                im, confidence, confidence, 0, False, max_det=10)
 
             targets = []
             for i, det in enumerate(pred):
@@ -122,17 +105,18 @@ def main():
                         s += f"{n} {int(c)}, "  # add to string
 
                     for *xyxy, conf, cls in reversed(det):
-                        targets.append((xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist() + [float(conf)])  # normalized xywh
+                        targets.append((xyxy2xywh(torch.tensor(xyxy).view(
+                            1, 4)) / gn).view(-1).tolist() + [float(conf)])  # normalized xywh
 
-            targets = pd.DataFrame(targets, columns=['current_mid_x', 'current_mid_y', 'width', "height", "confidence"])
+            targets = pd.DataFrame(
+                targets, columns=['current_mid_x', 'current_mid_y', 'width', "height", "confidence"])
 
             center_screen = [cWidth, cHeight]
             # If there are people in the center bounding box
             if len(targets) > 0:
                 if centerOfScreen:
                     # Compute the distance from the center
-                    targets["dist_from_center"] = np.sqrt((targets.current_mid_x - center_screen[0]) ** 2 +
-                                                           (targets.current_mid_y - center_screen[1]) ** 2)
+                    targets["dist_from_center"] = np.sqrt((targets.current_mid_x - center_screen[0])**2 + (targets.current_mid_y - center_screen[1])**2)
 
                     # Sort the data frame by distance from center
                     targets = targets.sort_values("dist_from_center")
@@ -142,99 +126,107 @@ def main():
                     targets['last_mid_x'] = last_mid_coord[0]
                     targets['last_mid_y'] = last_mid_coord[1]
                     # Take the distance between the current person's mid-coordinate and the last person's mid-coordinate
-                    targets['dist'] = np.linalg.norm(targets.iloc[:, [0, 1]].values - targets.iloc[:, [4, 5]], axis=1)
+                    targets['dist'] = np.linalg.norm(
+                        targets.iloc[:, [0, 1]].values - targets.iloc[:, [4, 5]], axis=1)
                     targets.sort_values(by="dist", ascending=False)
 
                 # Take the first person that shows up in the dataframe (Recall that we sort based on Euclidean distance)
                 xMid = targets.iloc[0].current_mid_x
                 yMid = targets.iloc[0].current_mid_y
+
                 box_height = targets.iloc[0].height
-            if headshot_mode:
-                headshot_offset = box_height * 0.37
-            else:
-                headshot_offset = box_height * 0.2
+                if headshot_mode:
+                    headshot_offset = box_height * 0.37
+                else:
+                    headshot_offset = box_height * 0.2
 
-            mouseMove = [xMid - cWidth, (yMid - headshot_offset) - cHeight]
+                mouseMove = [xMid - cWidth, (yMid - headshot_offset) - cHeight]
 
-            # Calculate distance from the center of the screen
-            dist_from_center = np.sqrt(mouseMove[0] ** 2 + mouseMove[1] ** 2)
+                # Calculate distance from the center of the screen
+                dist_from_center = np.sqrt(mouseMove[0]**2 + mouseMove[1]**2)
 
-            if fovCircle:
-                # Check if the target is within the FOV circle
-                if dist_from_center <= fovCircleSize:
+                if fovCircle:
+                    # Check if the target is within the FOV circle
+                    if dist_from_center <= fovCircleSize:
+                        # Moving the mouse
+                        if win32api.GetKeyState(0x02) < 0:  # Caps lock for toggle
+                            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(
+                                mouseMove[0] * aaMovementAmp), int(mouseMove[1] * aaMovementAmp), 0, 0)
+                    last_mid_coord = [xMid, yMid]
+
+                else:
                     # Moving the mouse
-                    if win32api.GetKeyState(0x02) < 0:  # Caps lock for toggle
+                    if win32api.GetKeyState(0x02) < 0:
                         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(
                             mouseMove[0] * aaMovementAmp), int(mouseMove[1] * aaMovementAmp), 0, 0)
-                last_mid_coord = [xMid, yMid]
+                    last_mid_coord = [xMid, yMid]
+
+                # Triggerbot    Alt for Toggle    Settings in config.py
+                if win32api.GetKeyState(0xA4) and abs(mouseMove[0]) <= aaTriggerBotWidth and abs(mouseMove[1]) <= aaTriggerBotHeight:
+                    # Press the mouse button
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                    # Release the mouse button
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
 
             else:
-                # Moving the mouse
-                if win32api.GetKeyState(0x02) < 0:
-                    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(
-                        mouseMove[0] * aaMovementAmp), int(mouseMove[1] * aaMovementAmp), 0, 0)
-                last_mid_coord = [xMid, yMid]
+                last_mid_coord = None
 
-            # Triggerbot    Alt for Toggle    Settings in config.py
-            if win32api.GetKeyState(0xA4) and abs(mouseMove[0]) <= aaTriggerBotWidth and abs(
-                    mouseMove[1]) <= aaTriggerBotHeight:
-                # Press the mouse button
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-                # Release the mouse button
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            # See what the bot sees
+            if visuals:
+                npImg = cp.asnumpy(npImg[0])
 
-        else:
-            last_mid_coord = None
+                # Display FOV Circle
+                if fovCircle:
+                    cv2.circle(npImg, (cWidth, cHeight), fovCircleSize, (0, 0, 255), 2)
 
-        # Capture any key press on the keyboard
-        if keyboard.is_pressed():
-            print("Key Pressed!")
+                # Loops over every item identified and draws a bounding box
+                for i in range(0, len(targets)):
+                    halfW = round(targets["width"][i] / 2)
+                    halfH = round(targets["height"][i] / 2)
+                    midX = targets['current_mid_x'][i]
+                    midY = targets['current_mid_y'][i]
+                    (startX, startY, endX, endY) = int(
+                        midX + halfW), int(midY + halfH), int(midX - halfW), int(midY - halfH)
 
-        # See what the bot sees
-        if visuals:
-            npImg = cp.asnumpy(npImg[0])
+                    idx = 0
+                    # draw the bounding box and label on the frame
+                    label = "{}: {:.2f}%".format(
+                        "Human", targets["confidence"][i] * 100)
+                    cv2.rectangle(npImg, (startX, startY), (endX, endY),
+                                  COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(npImg, label, (startX, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
-            # Display FOV Circle
-            if fovCircle:
-                cv2.circle(npImg, (cWidth, cHeight), fovCircleSize, (0, 0, 255), 2)
+            # Forced garbage cleanup every second
+            count += 1
+            if (time.time() - sTime) > 1:
+                if cpsDisplay:
+                    print("CPS: {}".format(count))
+                count = 0
+                sTime = time.time()
 
-            # Loops over every item identified and draws a bounding box
-            for i in range(0, len(targets)):
-                halfW = round(targets["width"][i] / 2)
-                halfH = round(targets["height"][i] / 2)
-                midX = targets['current_mid_x'][i]
-                midY = targets['current_mid_y'][i]
-                (startX, startY, endX, endY) = int(midX + halfW), int(midY + halfH), int(midX - halfW), int(midY - halfH)
+            # See visually what the Aimbot sees
+            if visuals:
+                # Check if the window exists
+                if not any(window.title == 'Live Feed' for window in gw.getWindowsWithTitle('Live Feed')):
+                    cv2.namedWindow('Live Feed', cv2.WINDOW_NORMAL)
+                    cv2.resizeWindow('Live Feed', cWidth, cHeight)
+                cv2.imshow('Live Feed', npImg)
+                live_feed_window = gw.getWindowsWithTitle('Live Feed')[0]
+                set_window_on_top(live_feed_window)
 
-                idx = 0
-                # draw the bounding box and label on the frame
-                label = "{}: {:.2f}%".format("Human", targets["confidence"][i] * 100)
-                cv2.rectangle(npImg, (startX, startY), (endX, endY), COLORS[idx], 2)
-                y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv2.putText(npImg, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                    exit()
 
-        # Forced garbage cleanup every second
-        count += 1
-        if (time.time() - sTime) > 1:
-            if cpsDisplay:
-                print("CPS: {}".format(count))
-            count = 0
-            sTime = time.time()
+    camera.stop()
 
-        # See visually what the Aimbot sees
-        if visuals:
-            cv2.imshow('Live Feed', npImg)
-
-            if (cv2.waitKey(1) & 0xFF) == ord('q'):
-                exit()
-
-camera.stop()
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
         import traceback
-        traceback.print_exception(e)
+        traceback.print_exception(type(e), e, e.__traceback__)
         print(str(e))
         print("Ask @Wonder for help in our Discord in the #ai-aimbot channel ONLY: https://discord.gg/rootkitorg")
